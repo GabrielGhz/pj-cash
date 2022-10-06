3842f227d4779382d8bc959f2673f457f431304e
Gb <joaogabrielmcpe110@gmail.com>
2022-10-06 15:44:05 -0300

    3 º commit nenhuma alteração

--- pj_transacoes.py
+++ pj_transacoes.py
@@ -1,3 +1,6 @@
+from cliente import cadastramento
+import random
+
 # import cliente
 # import conta_corrente
 # import minhas_transferencias
@@ -6,9 +9,6 @@
 # import meus_emprestimos
 
 # todas as funções referentes ao banco (empréstimo, depósito e saque)
-
-import cliente
-import random
 print("Olá, seja Bem - vindo ao banco PJ CASH")
 while True:
     p = int(input("Você deseja se cadastrar: \n 1-SIM \n 2-NÃO \n  "))
@@ -17,4 +17,4 @@ while True:
         break
     if p == 1:
         print("Preencha as informações abaixo: ")
-        cliente.cadastramento()
+        cadastramento()
