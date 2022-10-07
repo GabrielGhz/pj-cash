494f91190f64e9446c429449f0465750ff100ce0
Gb <joaogabrielmcpe110@gmail.com>
2022-10-07 11:20:32 -0300

    finalizado as transações

--- pj_transacoes.py
+++ pj_transacoes.py
@@ -9,12 +9,38 @@ import random
 # import meus_emprestimos
 
 # todas as funções referentes ao banco (empréstimo, depósito e saque)
+import cliente
+import random
+
 print("Olá, seja Bem - vindo ao banco PJ CASH")
 while True:
-    p = int(input("Você deseja se cadastrar: \n 1-SIM \n 2-NÃO \n  "))
-    if p == 2:
+    cad = int(input("Você deseja se cadastrar: \n1-SIM \n2-NAO \n:"))
+    if cad == 2:
         print("OBRIGADO PELA VISITA!")
         break
-    if p == 1:
+    if cad == 1:
         print("Preencha as informações abaixo: ")
         cliente.cadastramento()
+    cc = int(input("Voce deseja criar uma conta corrente? \n1-SIM \n2-NAO \n:"))
+    if cc == 2:
+        for x in range(9999):
+            dep = int(input("Você deseja realizar um deposito? \n1-SIM \n2-NAO \n:"))
+            if dep == 2:
+                empr = int(input("Você deseja realizar um emprestimo? \n1-SIM \n2-NAO \n:"))
+                if empr == 2:
+                    break
+                if empr == 1:
+                    print('funcao emprestimo')                    
+                break
+            if dep == 1:
+                print('funcao deposito')
+    if cc == 1:
+        print("Preencha as informações abaixo: ")
+        input("INFORMAÇÔES TESTE")
+    dep = int(input("Você deseja depositar, sacar ou nenhuma das anteriores?: \n1-depositar \n2-sacar \n3-nenhuma das anteriores \n:"))    
+    if dep == 1:
+        print('funcao depositar')
+    elif dep == 2:
+        print('funcao sacar')
+    else:
+        break
